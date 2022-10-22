7569846dc59295eb5f17d30e06ab161e553017c3
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-21 21:14:05 -0700

    Create the badges fxns

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,25 +1,46 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "n/a"){
+    return "![GitHub license](https://img.shields.io/badge/License-"${license}"-blue.svg)"
+  } return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = ""
+  if (license !== ""){
+    let result = license.toLowerCase()
+    return licenseLink = "https://choosealicense.com/licenses/"+result
+  }else{
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== ""){
+    return `\n- [License](#license)\n`;
+    } return "";
+  }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   # Project Title
     <img src="https://img.shields.io/badge/LICENSE-${data.license}-COLOR.svg?logo=LOGO">
+
     ## Description
     ${data.description}
     ## Table of Contents (Optional)
     - [Installation](#installation)
     - [Usage](#usage)
+    - [License](#license)
     - [Contributions](#contributing)
     - [Tests](#tests)
     - [Questions](#questions)
